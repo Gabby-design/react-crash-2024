d86e473ba2475e41115e5db212a798e66824b3c2
Gabby <gabrieltolulope604@gmail.com>
2025-02-12 02:53:41 +0100

    the base url chaged

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-crash-2024/",
+  // base: "/react-crash-2024/",
   plugins: [react()],
   server: {
     port: 3000,
